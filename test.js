8f08852eae3e3d61a0551876269bc273879e2f45
abderhman <abdohfny21000@gmail.com>
2022-12-11 21:42:11 +0200

    calc

--- test.js
+++ test.js
@@ -1,2 +1,29 @@
-let a = "Elzero web school"
-console.log(a.toLowerCase().charAt() + a.toUpperCase().slice(1 , length - 1) + a.toLowerCase().slice(-1));
+let n1 = window.prompt("enter the first num");
+
+let con = window.prompt("enter the operator");
+
+let n2 = window.prompt("enter the scande num");
+var er = "ادخل رقم صحيح";
+
+if (con == "+") {
+  document.write(+n1 + +n2);
+}
+else if (con == "+") {
+  document.write(+n1 + +n2);
+}
+else if (con == "-") {
+  document.write(+n1 - +n2);
+}
+else if (con == "*") {
+  document.write(+n1 * +n2);
+}
+else if (con == "/") {
+  document.write(+n1 / +n2);
+}
+else if (con == "%") {
+  document.write(+n1 % +n2);
+}
+else if (con == "**") {
+  document.write(n1 ** +n2);
+}
+else {document.write(er)};
