22b4be6831af8a04a4957260e20400865dddd706
abderhman <abdohfny21000@gmail.com>
2022-12-11 16:21:44 +0200

    a

--- test.js
+++
@@ -1,7 +0,0 @@
-let a = "Elzero web school";
-let l = "z";
-let s = "e";
-let d = "o";
-console.log(a.includes(s.toUpperCase()));
-console.log(a.includes(l.toUpperCase(), 2));
-console.log(a.includes(d.toUpperCase(), -2));
