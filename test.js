386389431f5ea3f6ca91d4f3a19df3c270bbaa63
abderhman <abdohfny21000@gmail.com>
2022-12-15 23:18:20 +0200

    a

--- test.js
+++ test.js
@@ -1,12 +1,14 @@
-let a = [1, 2, "ahmed", "khaled", "mohamed", "ana", 3];
-let b = [];
+let pr = ["iphone", "ipad", "telphon", "mack", "labtob", "tv"];
+let co = ["red", "black", "bleu"];
+let count = 4;
+document.write(`<h1>Show [ ${count} ] producte</h1>`);
+for (let i = 0; i < count; i++) {
+  document.write(`<div>`);
+  document.write(`<h3>${pr[i]}</h3>`);
 
-
-for (let i = 0; i < a.length; i++) {
-  if (typeof a[i] === typeof"String") {
-     b.push(a[i]);
+  for (let j = 0; j < co.length; j++) {
+    document.write(`<p>${co[j]}</p>`);
+  }
+  document.write(`</div>`);
+  document.write(`<p>${co.join(" | ")}</p>`);
 }
-}
-
-console.log(b);
-
