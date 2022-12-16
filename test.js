d159d67649007409e861702c9f901bd6c4f19ac3
abderhman <abdohfny21000@gmail.com>
2022-12-16 10:55:55 +0200

    a

--- test.js
+++ test.js
@@ -1,14 +1,14 @@
 let pr = ["iphone", "ipad", "telphon", "mack", "labtob", "tv"];
 let co = ["red", "black", "bleu"];
 let count = 4;
-document.write(`<h1>Show [ ${count} ] producte</h1>`);
-for (let i = 0; i < count; i++) {
-  document.write(`<div>`);
-  document.write(`<h3>${pr[i]}</h3>`);
 
-  for (let j = 0; j < co.length; j++) {
-    document.write(`<p>${co[j]}</p>`);
-  }
-  document.write(`</div>`);
-  document.write(`<p>${co.join(" | ")}</p>`);
+
+function sayhi(user) {
+  console.log(`hi ${user}`);
+}
+
+
+for (let i = 0; i < pr.length; i++) {
+  sayhi(`${pr[i]}`)
+  
 }
