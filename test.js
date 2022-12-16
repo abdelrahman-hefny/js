31e078e2dc373e21c357e5225b48104d8049dc2a
abderhman <abdohfny21000@gmail.com>
2022-12-16 17:51:16 +0200

    advanced_fun

--- test.js
+++ test.js
@@ -1,9 +1,29 @@
-function calc(...numbers) {
-  let a = 0;
-  for (let i = 0; i < numbers.length; i++) {
-    a += numbers[i];
-  }
-  return a;
-}
-console.log(calc(10 , 20 ,4));
+function pr(us, age, pr, show = "Yes", ...sk) {
+  document.write(`<div>`);
+  document.write(`<h1>Hello ${us}</h1>`);
+  document.write(`<p>age ${age}</p>`);
+  document.write(`<p>sallare $${pr}</p>`);
 
+
+
+
+  if (show === "Yes") {
+
+    if (sk.length > 0) {
+    document.write(`<p>${sk.join(" | ")}</p>`);
+    }
+    
+     else{document.write( `<p>there no skills</p>`)};
+
+
+  } else {document.write(`<p>do not show skills</p>`)};
+
+
+
+
+
+
+  document.write(`</div>`);
+}
+
+pr("abdo", 19, 10, "Yes", "html ", "css " , "ad");
