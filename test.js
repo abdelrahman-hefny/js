058c0c5a3a7d377e014e0d1ca5b1627fbbe0fe53
abderhman <abdohfny21000@gmail.com>
2022-12-18 13:57:55 +0200

    a

--- test.js
+++ test.js
@@ -1,14 +1,19 @@
-let mys = "1,2,3,EE,l,z,e,r,o,_,W,e,b,_,S,c,h,o,o,l,1,2,z";
+let v = true;
 
-let solutoin = mys
-  .split("")
-  .map((ele) => (ele !== "," ? ele : ""))
-  .map((ele) => (ele !== "_" ? ele : " "))
-  .filter((ele) => isNaN(parseInt(ele)))
-  .reduce((acc, carnt) => acc + carnt)
-  .slice(true, -isNaN(mys));
-
-// solutoin[solutoin.length - 1] = "";
-// solutoin[3] = "";
-
-console.log(solutoin);
+let user = {
+  name: "ahmed",
+  va: false,
+  conter: {
+    egept: {
+      one: "airo",
+      tow: "egeza\n a",
+    },
+  },
+  //me
+  chich: function () {
+    if (user.va === true) {
+      return `free`;
+    } else return ` not free`; 
+  },
+};
+console.log(user.chich());
