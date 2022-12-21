8c80f63fd4787003e36d6daeace78272de71a54f
abderhman <abdohfny21000@gmail.com>
2022-12-21 15:43:47 +0200

    clack

--- test.js
+++ test.js
@@ -1,32 +1,10 @@
-// Select the body element
-const body = document.querySelector("body");
-
-// Create a new heading element
-const heading = document.createElement("h1");
-
-// Set the text of the heading
-heading.textContent = "Welcome to my website!";
-
-// Add the heading to the body
-body.appendChild(heading);
-
-// heading.onclick = one;
-// heading.onclick = tow;
-let a = document.createElement("p")
-a.textContent = "abdo"
-function one() {
-  console.log("ih 1");
+let a = setTimeout(po, 3000);
+function po() {
+  console.log(`abdo`);
 }
-function tow() {
-  console.log("ih 2");
+console.log(a);
+let btn = document.querySelector("button");
+
+btn.onclick = function(){
+    clearTimeout(a)
 }
-heading.onclick = function () {
-  np = heading.cloneNode(true);
-  np.className = "newp";
-  body.appendChild(np);
-  document.addEventListener("click", function (e) {
-    if (e.target.className === 'newp') {
-      body.appendChild(a)
-    }
-  });
-};
