947931433c6923426b5fb322cfc2ad51899d7b0e
abderhman <abdohfny21000@gmail.com>
2022-12-21 11:52:55 +0200

    click in colne ELE

--- test.js
+++ test.js
@@ -1,8 +1,32 @@
-let ele = document.getElementById("p");
+// Select the body element
+const body = document.querySelector("body");
 
-console.log(ele.classList[0]);
-console.log(ele.classList.contains("one"));
-console.log(ele.classList.item("2"));
-ele.onclick = function () {
-  ele.classList.toggle("onde");
+// Create a new heading element
+const heading = document.createElement("h1");
+
+// Set the text of the heading
+heading.textContent = "Welcome to my website!";
+
+// Add the heading to the body
+body.appendChild(heading);
+
+// heading.onclick = one;
+// heading.onclick = tow;
+let a = document.createElement("p")
+a.textContent = "abdo"
+function one() {
+  console.log("ih 1");
+}
+function tow() {
+  console.log("ih 2");
+}
+heading.onclick = function () {
+  np = heading.cloneNode(true);
+  np.className = "newp";
+  body.appendChild(np);
+  document.addEventListener("click", function (e) {
+    if (e.target.className === 'newp') {
+      body.appendChild(a)
+    }
+  });
 };
